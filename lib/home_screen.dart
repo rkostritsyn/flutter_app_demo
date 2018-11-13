a53c7db350d4377c7442ece9f4ce9d2c9952ff83
Roman <Roman.Kostritsyn@macys.com>
2018-11-13 22:45:40 +0200

    add promo banners, offers

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_demo/models.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen(this.listType);
@@ -28,9 +29,12 @@ class HomeScreen extends StatelessWidget {
                       ),
                     )
                   ),
-                  margin: EdgeInsets.all(10),
-                )
-
+                  margin: EdgeInsets.all(20),
+                ),
+                new PromoBanner(bannerPath: 'assets/black_friday_banner.png'),
+                new CategoryYouShopped(),
+                new PromoBanner(bannerPath: 'assets/best_of.png',),
+                new OffersList()
               ],
             ),
           ),
@@ -40,3 +44,105 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
+class PromoBanner extends StatelessWidget {
+  const PromoBanner({this.bannerPath});
+  final String bannerPath;
+
+  @override
+  Widget build(BuildContext context) {
+    var blackFridayBannerImage = new AssetImage(bannerPath);
+    var banner = new Image(image: blackFridayBannerImage, fit: BoxFit.cover,);
+    return new Container(child: banner);
+  }
+}
+
+class OffersList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Card(
+        margin: EdgeInsets.all(20),
+          child: new Column(children: <Widget>[
+            new OffersViewHeader(),
+            new Divider(color: Colors.black26, height: 2,),
+            new OfferViewer(offerModel: new OfferModel(offerName: "Offer name 0", offerDescription: "Offer"),),
+            new Divider(color: Colors.black26, height: 2,),
+            new OfferViewer(offerModel: new OfferModel(offerName: "Offer name 1", offerDescription: "Offer"),),
+            new Divider(color: Colors.black26, height: 2,),
+            new OfferViewer(offerModel: new OfferModel(offerName: "Offer name 2", offerDescription: "Offer"),),
+            new Divider(color: Colors.black26, height: 2,),
+            new OfferViewer(offerModel: new OfferModel(offerName: "Offer name 3", offerDescription: "Offer"),),
+          ],
+
+          ),
+        ),
+
+    );
+  }
+}
+
+class OffersViewHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      constraints: BoxConstraints.expand(height: 30),
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[new Icon(Icons.ac_unit), new Text("Offers")],),);
+  }
+}
+
+class OfferViewer extends StatelessWidget {
+  final OfferModel offerModel;
+  const OfferViewer({this.offerModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      constraints: BoxConstraints.expand(height: 40),
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Text(offerModel.offerName),
+          new Text(offerModel.offerDescription)
+        ],
+      )
+    );
+  }
+}
+
+class CategoryYouShopped extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      margin: const EdgeInsets.all(10),
+      child: Column(
+
+
+      ),
+    );
+  }
+}
+
+class ShopCategoryViewer extends StatelessWidget {
+  final ShopCategoryModel category;
+  const ShopCategoryViewer({this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: Column(
+
+        children: <Widget>[
+          new Image(image: new AssetImage(''),  fit: BoxFit.cover,),
+
+        ],
+      ),
+    ) ;
+  }}
---
+++ lib/models.dart
@@ -0,0 +1,13 @@
+class OfferModel {
+  final String offerName;
+  final String offerDescription;
+
+  const OfferModel({this.offerName, this.offerDescription});
+}
+
+class ShopCategoryModel {
+  final String name;
+  final String image;
+
+  const ShopCategoryModel({this.name, this.image});
+}
